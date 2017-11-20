29832ab7b69ffa13b91b5e4e6d352dffb715b54e
yanyongjun <yanyojun@foxmail.com>
2017-11-20 20:08:47 +0800

    图库界面优化

--- app/src/main/java/com/isoftstone/smartsite/model/dirtcar/Data/SelectImage.kt
+++ app/src/main/java/com/isoftstone/smartsite/model/dirtcar/Data/SelectImage.kt
@@ -3,4 +3,17 @@ package com.isoftstone.smartsite.model.dirtcar.Data
 /**
  * Created by yanyongjun on 2017/11/19.
  */
-data class SelectImage(var path:String, var stats:Boolean)
+open class SelectImage(path: String, stats: Boolean, listener: OnStatusChangeListener) {
+    var mPath = path
+    var mListener = listener
+    var mStatus = stats
+        set(temp) {
+            field = temp
+            mListener?.onChange(field,mPath)
+        }
+
+
+    interface OnStatusChangeListener {
+        fun onChange(status: Boolean,path:String)
+    }
+}
