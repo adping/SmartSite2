63c777ec2a29742a38b411102d1c12572ad6b8d2
yanyongjun <yanyojun@foxmail.com>
2017-11-22 21:43:03 +0800

    1、跳转到拍照取证界面

--- app/src/main/java/com/isoftstone/smartsite/model/dirtcar/adapter/DirtCarAdapter.kt
+++ app/src/main/java/com/isoftstone/smartsite/model/dirtcar/adapter/DirtCarAdapter.kt
@@ -1,13 +1,14 @@
 package com.isoftstone.smartsite.model.dirtcar.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
 import android.widget.BaseAdapter
 import com.isoftstone.smartsite.R
 import com.isoftstone.smartsite.model.dirtcar.Data.DirtcarBean
+import com.isoftstone.smartsite.model.dirtcar.activity.ManualPhotographyActivity
 
 /**
  * Created by yanyongjun on 2017/11/14.
@@ -24,6 +25,13 @@ open class DirtCarAdapter(context: Context, datas: ArrayList<DirtcarBean>) : Bas
         var v  = if (convertView != null) convertView else {
             LayoutInflater.from(mContext).inflate(R.layout.listview_dirtcar_item, null)
         }
+        var vCamera = v.findViewById(R.id.linear_camera)
+        vCamera.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                var i =  Intent(mContext,ManualPhotographyActivity::class.java)
+                mContext.startActivity(i)
+            }
+        })
         return v
     }
 
