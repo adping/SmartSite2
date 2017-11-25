a882e1fad12789f0ae27256cf9b7ffaf9ca46d3d
gone <w380850629@163.com>
2017-11-25 12:14:04 +0800

    1、调整首页图标

--- app/src/main/java/com/isoftstone/smartsite/model/main/ui/MainFragment.java
+++ app/src/main/java/com/isoftstone/smartsite/model/main/ui/MainFragment.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.isoftstone.smartsite.MainActivity;
 import com.isoftstone.smartsite.PatroPlanDetailsActivity;
@@ -25,22 +26,24 @@ import com.isoftstone.smartsite.model.inspectplan.activity.PatrolPlanActivity;
 import com.isoftstone.smartsite.model.map.ui.ConstructionMonitorMapActivity;
 import com.isoftstone.smartsite.model.tripartite.activity.TripartiteActivity;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 
 /**
  * Created by zw on 2017/10/11.
- *
- *
+ * <p>
+ * <p>
  * 开发版SHA1 ： 17:02:19:67:57:D4:F4:AF:3E:AE:22:1F:95:65:9A:27:FD:F7:8D:D0
- *
- *             17:02:19:67:57:D4:F4:AF:3E:AE:22:1F:95:65:9A:27:FD:F7:8D:D0;com.isoftstone.smartsite
- *
- *
+ * <p>
+ * 17:02:19:67:57:D4:F4:AF:3E:AE:22:1F:95:65:9A:27:FD:F7:8D:D0;com.isoftstone.smartsite
+ * <p>
+ * <p>
  * 发布版SHA1 ： C3:83:83:56:68:FD:2B:BC:EE:BB:16:AF:BA:52:EC:6A:C9:24:19:D5
- *
- *              C3:83:83:56:68:FD:2B:BC:EE:BB:16:AF:BA:52:EC:6A:C9:24:19:D5;com.isoftstone.smartsite
+ * <p>
+ * C3:83:83:56:68:FD:2B:BC:EE:BB:16:AF:BA:52:EC:6A:C9:24:19:D5;com.isoftstone.smartsite
  */
 
-public class MainFragment extends BaseFragment{
+public class MainFragment extends BaseFragment {
 
     private TextView mCityTestView = null;
     private TextView mWeatherTextView = null;
@@ -61,15 +64,15 @@ public class MainFragment extends BaseFragment{
     private LinearLayout mUnCheckMsg = null;            //视频监控设备
     private LinearLayout mUntreatedReport = null;       //环境监控设备
     private ListView mListView = null;
-    private ImageView  wuran_image = null;
+    private ImageView wuran_image = null;
     private ImageView wuran_icon = null;
     private TextView wuran_text = null;
     private TextView wuran_number = null;
     private TextView shidu_textview = null;
     private TextView fengxiang_textview = null;
 
-    public static final  int HANDLER_GET_HOME_DATA_START = 1;
-    public static final  int HANDLER_GET_HOME_DATA_END = 2;
+    public static final int HANDLER_GET_HOME_DATA_START = 1;
+    public static final int HANDLER_GET_HOME_DATA_END = 2;
     private MobileHomeBean mMobileHomeBean = null;
     private View mConstructionMonitor;
 
@@ -83,7 +86,7 @@ public class MainFragment extends BaseFragment{
         initView();
     }
 
-    private void initView(){
+    private void initView() {
         mCityTestView = (TextView) rootView.findViewById(R.id.text_city);
         lab_main_unread_num = (TextView) rootView.findViewById(R.id.lab_main_unread_num);  //未查看消息数目
         lab_main_unread_num.setVisibility(View.INVISIBLE);
@@ -93,7 +96,7 @@ public class MainFragment extends BaseFragment{
         lab_vcr_unread_num.setVisibility(View.INVISIBLE);
         lab_air_unread_num = (TextView) rootView.findViewById(R.id.lab_air_unread_num);//环境监控数目
         lab_air_unread_num.setVisibility(View.INVISIBLE);
-        mTemperatureTextView = (TextView)  rootView.findViewById(R.id.text_temperature);
+        mTemperatureTextView = (TextView) rootView.findViewById(R.id.text_temperature);
         mUnCheckMsg = (LinearLayout) rootView.findViewById(R.id.textView10);
         mUnCheckMsg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -122,6 +125,7 @@ public class MainFragment extends BaseFragment{
                 enterAirMonitoringMsg();
             }
         });
+        //视频监控
         mVideoMonitoring = rootView.findViewById(R.id.button_1);
         mVideoMonitoring.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -129,6 +133,7 @@ public class MainFragment extends BaseFragment{
                 enterVideoMonitoring();
             }
         });
+        //环境监控
         mAirMonitoring = rootView.findViewById(R.id.button_2);
         mAirMonitoring.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -136,6 +141,7 @@ public class MainFragment extends BaseFragment{
                 enterAirMonitoring();
             }
         });
+        //三方协同
         mThirdPartReport = rootView.findViewById(R.id.button_3);
         mThirdPartReport.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -143,6 +149,45 @@ public class MainFragment extends BaseFragment{
                 enterThirdPartReport();
             }
         });
+        // 渣土车监控
+        mDircar = rootView.findViewById(R.id.button_4);
+        mDircar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterDircar();
+            }
+        });
+        //巡查概况
+        rootView.findViewById(R.id.button_5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterPatrolSurvey();
+            }
+        });
+        //巡查计划
+        mInspectPlan = rootView.findViewById(R.id.button_6);
+        mInspectPlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterInspectPlan();
+            }
+        });
+        //巡查任务
+        rootView.findViewById(R.id.button_7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterPatrolMission();
+            }
+        });
+        //巡查监控
+        mConstructionMonitor = rootView.findViewById(R.id.button_8);
+        mConstructionMonitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterConstructionMonitor();
+            }
+        });
+
         mListView = (ListView) rootView.findViewById(R.id.list);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -156,49 +201,15 @@ public class MainFragment extends BaseFragment{
         wuran_number = (TextView) rootView.findViewById(R.id.wuran_number);
         shidu_textview = (TextView) rootView.findViewById(R.id.shidu_textview);
         fengxiang_textview = (TextView) rootView.findViewById(R.id.fengxiang_textview);
-
-        mInspectPlan = rootView.findViewById(R.id.button_6);
-        mInspectPlan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enterInspectPlan();
-            }
-        });
-
-        mConstructionMonitor = rootView.findViewById(R.id.button_8);
-        mConstructionMonitor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enterConstructionMonitor();
-            }
-        });
-
-        // 渣土车监控
-        mDircar = rootView.findViewById(R.id.button_4);
-        mDircar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enterDircar();
-            }
-        });
-
-        //巡查任务
-        rootView.findViewById(R.id.button_7).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enterPatrolMission();
-            }
-        });
     }
 
 
-
-    private Handler mHandler = new Handler(){
+    private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
-                case HANDLER_GET_HOME_DATA_START:{
-                    Thread thread = new Thread(){
+            switch (msg.what) {
+                case HANDLER_GET_HOME_DATA_START: {
+                    Thread thread = new Thread() {
                         @Override
                         public void run() {
                             mMobileHomeBean = mHttpPost.getMobileHomeData();
@@ -207,66 +218,66 @@ public class MainFragment extends BaseFragment{
                     };
                     thread.start();
                 }
-                    break;
-                case HANDLER_GET_HOME_DATA_END:{
+                break;
+                case HANDLER_GET_HOME_DATA_END: {
                     setMobileHomeDataToView();
                 }
-                    break;
+                break;
             }
         }
     };
 
-    private void setWeatherData(){
-          //设置天气情况
+    private void setWeatherData() {
+        //设置天气情况
         DataQueryBean dataQueryBean = mMobileHomeBean.getAvgEqis();
-        if(dataQueryBean != null){
-            int  AQI = mMobileHomeBean.getAQI();
-            if(AQI < 50){
+        if (dataQueryBean != null) {
+            int AQI = mMobileHomeBean.getAQI();
+            if (AQI < 50) {
                 wuran_image.setBackgroundResource(R.drawable.wuran_you_jingdu);
                 wuran_icon.setBackgroundResource(R.drawable.main_aqi);
                 wuran_text.setText("优");
-            }else if(AQI < 100 && AQI >= 50){
+            } else if (AQI < 100 && AQI >= 50) {
                 wuran_image.setBackgroundResource(R.drawable.wuran_liang_jingdu);
                 wuran_icon.setBackgroundResource(R.drawable.main_aqi);
                 wuran_text.setText("良");
-            }else if(AQI < 200 && AQI >= 100){
+            } else if (AQI < 200 && AQI >= 100) {
                 wuran_image.setBackgroundResource(R.drawable.wuran_qingdu_jingdu);
                 wuran_icon.setBackgroundResource(R.drawable.main_aqi);
                 wuran_text.setText("轻度污染");
-            }else if(AQI < 300 && AQI >= 200){
+            } else if (AQI < 300 && AQI >= 200) {
                 wuran_image.setBackgroundResource(R.drawable.wuran_zhong1du_jingdu);
                 wuran_icon.setBackgroundResource(R.drawable.main_aqi);
                 wuran_text.setText("中度污染");
-            }else if(AQI >= 300){
+            } else if (AQI >= 300) {
                 wuran_image.setBackgroundResource(R.drawable.wuran_zhongdu_jingdu);
                 wuran_icon.setBackgroundResource(R.drawable.main_aqi);
                 wuran_text.setText("重度污染");
             }
-            wuran_number.setText(""+AQI);
-            mTemperatureTextView.setText(dataQueryBean.getAirTemperature()+"");
-            shidu_textview.setText(dataQueryBean.getAirHumidity()+"%");
+            wuran_number.setText("" + AQI);
+            mTemperatureTextView.setText(dataQueryBean.getAirTemperature() + "");
+            shidu_textview.setText(dataQueryBean.getAirHumidity() + "%");
             fengxiang_textview.setText(dataQueryBean.getWindDirection());
         }
     }
 
-    private void setMobileHomeDataToView(){
+    private void setMobileHomeDataToView() {
         mCityTestView.setText("武汉");
-        if(mMobileHomeBean == null){
+        if (mMobileHomeBean == null) {
             return;
         }
         int unreadMessage = mMobileHomeBean.getUnreadMessages();
-        if(unreadMessage > 0){
+        if (unreadMessage > 0) {
             lab_main_unread_num.setVisibility(View.VISIBLE);
-            lab_main_unread_num.setText(unreadMessage+"");
+            lab_main_unread_num.setText(unreadMessage + "");
         }
         int unreadPatrols = mMobileHomeBean.getUntreatedPatrols();
-        if(unreadPatrols > 0){
+        if (unreadPatrols > 0) {
             lab_report_unread_num.setVisibility(View.VISIBLE);
-            lab_report_unread_num.setText(unreadPatrols+"");
+            lab_report_unread_num.setText(unreadPatrols + "");
         }
-        lab_vcr_unread_num.setText(mMobileHomeBean.getAllVses()+"");//视频监控设备数
+        lab_vcr_unread_num.setText(mMobileHomeBean.getAllVses() + "");//视频监控设备数
         lab_vcr_unread_num.setVisibility(View.VISIBLE);
-        lab_air_unread_num.setText(mMobileHomeBean.getAllEmes()+"");//环境监控数目
+        lab_air_unread_num.setText(mMobileHomeBean.getAllEmes() + "");//环境监控数目
         lab_air_unread_num.setVisibility(View.VISIBLE);
         //设置天气参数
         setWeatherData();
@@ -277,8 +288,7 @@ public class MainFragment extends BaseFragment{
     }
 
 
-
-    private void onItemClicked(){
+    private void onItemClicked() {
 //        ThreePartyData data = new ThreePartyData();
 //        data.setType(ThreePartyData.TYPE_RECEIVE_REPORT);
 //        data.setName("张珊");
@@ -288,21 +298,22 @@ public class MainFragment extends BaseFragment{
 //        getActivity().startActivity(intent);
     }
 
-    private void enterUnChekMsg(){
-          //进入未查看消息
-        ((MainActivity)getActivity()).setCurrentTab(2);
+    private void enterUnChekMsg() {
+        //进入未查看消息
+        ((MainActivity) getActivity()).setCurrentTab(2);
     }
 
-    private void enterUntreatedReport(){
+    private void enterUntreatedReport() {
         //进入未处理报告
-        ((MainActivity)getActivity()).setCurrentTab(2);
+        ((MainActivity) getActivity()).setCurrentTab(2);
     }
 
-    private void enterAirMonitoringMsg(){
-        Intent intent = new Intent(getActivity(),PMDevicesListActivity.class);
+    private void enterAirMonitoringMsg() {
+        Intent intent = new Intent(getActivity(), PMDevicesListActivity.class);
         getActivity().startActivity(intent);
     }
-    private void enterThirdPartReport(){
+
+    private void enterThirdPartReport() {
         //进入三方协同
         /*((MainActivity)getActivity()).setCurrentTab(2);*/
         //yanyongjun 这个地方应该是进到三方协同界面，而不是到消息fragment页
@@ -310,48 +321,55 @@ public class MainFragment extends BaseFragment{
         startActivity(intent);
     }
 
-    private void enterVideoMonitoring(){
+    private void enterVideoMonitoring() {
         //进入视频监控
-        Intent intent = new Intent(getActivity(),VideoMonitoringActivity.class);
+        Intent intent = new Intent(getActivity(), VideoMonitoringActivity.class);
         getActivity().startActivity(intent);
     }
 
     /**
      * 进入渣土车监控
      */
-    private void enterDircar(){
-        Intent intent = new Intent(getActivity(),SlagcarInfoActivity.class);
+    private void enterDircar() {
+        Intent intent = new Intent(getActivity(), SlagcarInfoActivity.class);
         getActivity().startActivity(intent);
     }
+    /*
+     进入巡查概况
+     */
+    private void enterPatrolSurvey(){
+        Toast.makeText(getContext(),"进入巡查概况",Toast.LENGTH_SHORT).show();
+    }
 
-    private void enterAirMonitoring(){
+    private void enterAirMonitoring() {
         //进入环境监控
-        Intent intent = new Intent(getActivity(),AirMonitoringActivity.class);
+        Intent intent = new Intent(getActivity(), AirMonitoringActivity.class);
         getActivity().startActivity(intent);
     }
 
     private void enterInspectPlan() {
         //进入巡查计划
-        if(HttpPost.mLoginBean.getmUserBean().getmPermission().isM_CPPA()){
+        if (HttpPost.mLoginBean.getmUserBean().getmPermission().isM_CPPA()) {
             //有审批计划权限
-            Intent intent = new Intent(getActivity(),ApprovalPendingInspectPlansActivity.class);
+            Intent intent = new Intent(getActivity(), ApprovalPendingInspectPlansActivity.class);
             getActivity().startActivity(intent);
-        }else {
+        } else {
             //没有审批计划权限
-            Intent intent = new Intent(getActivity(),PatrolPlanActivity.class);
+            Intent intent = new Intent(getActivity(), PatrolPlanActivity.class);
             getActivity().startActivity(intent);
         }
 
     }
 
-    private  void enterPatrolMission(){
+    private void enterPatrolMission() {
         //进入巡查任务
-        Intent intent = new Intent(getActivity(),PatroPlanDetailsActivity.class);
+        Intent intent = new Intent(getActivity(), PatroPlanDetailsActivity.class);
         getActivity().startActivity(intent);
     }
-    private void enterConstructionMonitor(){
+
+    private void enterConstructionMonitor() {
         //进入施工监控
-        Intent intent = new Intent(getActivity(),ConstructionMonitorMapActivity.class);
+        Intent intent = new Intent(getActivity(), ConstructionMonitorMapActivity.class);
         getActivity().startActivity(intent);
     }
 
