7fd70e48eda22ab696fa08803ccb4710b8200ed8
niejixiangde <2580076280@qqccom>
2017-11-26 22:31:48 +0800

    niejixiang:gengxin 20171126

--- app/src/main/java/com/isoftstone/smartsite/utils/SharedPreferencesUtils.java
+++ app/src/main/java/com/isoftstone/smartsite/utils/SharedPreferencesUtils.java
@@ -1,6 +1,7 @@
 package com.isoftstone.smartsite.utils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 
 /**
@@ -8,19 +9,34 @@ import android.content.SharedPreferences;
  */
 
 public class SharedPreferencesUtils {
-    private static  final  String SHARED_PREFERENCES_NAME = "isoftstone";
-    private static  final  String SAVE_PASSWD = "save_passwd";
+    private static final String SHARED_PREFERENCES_NAME = "isoftstone";
+    private static final String SAVE_PASSWD = "save_passwd";
+    private static final String BASE_WIDTH = "base_width";
 
-    public static void updateSavePasswd(Activity activity,boolean isSave){
+    public static void updateSavePasswd(Activity activity, boolean isSave) {
         SharedPreferences settings = activity.getSharedPreferences(SHARED_PREFERENCES_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean(SAVE_PASSWD, isSave);
         editor.commit();
     }
 
-    public static  boolean getSavePasswd(Activity activity){
+    public static boolean getSavePasswd(Activity activity) {
         SharedPreferences settings = activity.getSharedPreferences(SHARED_PREFERENCES_NAME, 0);
-        boolean isSave = settings.getBoolean(SAVE_PASSWD,true);
-        return  isSave;
+        boolean isSave = settings.getBoolean(SAVE_PASSWD, true);
+        return isSave;
     }
+
+    public static void saveBaseWidth(Context context, int w) {
+        SharedPreferences settings = context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(BASE_WIDTH, w);
+        editor.commit();
+    }
+
+
+    public static int getBaseWidth(Context context) {
+        SharedPreferences settings = context.getSharedPreferences(SHARED_PREFERENCES_NAME, 0);
+        return settings.getInt(BASE_WIDTH, 0);
+    }
+
 }
